23a62c88b24a1e99676fd3075b3e3ad589c12ad8
vince <vince.lee@skywatch24.com>
2016-06-08 16:35:00 +0800

    init commit

---
+++ app/src/main/java/ntu/tw/alon21034/spheremodelcontroller/MainActivity.java
@@ -0,0 +1,13 @@
+package ntu.tw.alon21034.spheremodelcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
